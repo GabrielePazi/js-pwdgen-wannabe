d440e4ff8a1d0794ed0df7f69f0cc29abb7097df
GabrielePazi <pazigabriele@gmail.com>
2023-06-29 18:07:30 +0200

    1.1 aggiunte di design

--- javascript/main.js
+++ javascript/main.js
@@ -3,4 +3,9 @@
   const favColor = prompt("Inserire il proprio colore preferito")
   const age = parseInt(prompt("Inserisci la tua età"))
 
+  document.getElementById("firstName").innerHTML = firstName
+  document.getElementById("lastName").innerHTML = lastName
+  document.getElementById("favColor").innerHTML = favColor
+  document.getElementById("birthYear").innerHTML = 2023 - age
+
   document.getElementById("myPassword").innerHTML = firstName + lastName + favColor + (age + 21)
